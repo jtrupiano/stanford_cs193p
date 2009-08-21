b6a53771ea6adf352a9792c26263d8ab2c1fb22b
John Trupiano <jtrupiano@gmail.com>
2009-08-20 20:27:44 -0400

    Section 3 (my version of it at least) of assignment 1b

--- stanford.m
+++ stanford.m
@@ -1,21 +1,50 @@
 #import <Foundation/Foundation.h>
 
+void section1();
+void section2();
+void section3();
+
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
+	section1();
+	section2();
+	section3();
+		
+    [pool drain];
+    return 0;
+}
+
+void section1() {
 	NSString *path = @"~";
 	
 	path = [path stringByStandardizingPath];
-    // insert code here...
+    NSLog(@"My home folder is at '%@'", path);
 	
 	for (NSString *path_part in [path pathComponents]) {
 		NSLog(@"%@", path_part);
 	}
+}
 
-    NSLog(@"My home folder is at '%@'", path);
-	
+void section2() {
 	NSLog(@"Process Name: '%@' Process ID: '%d'", [[NSProcessInfo processInfo] processName], [[NSProcessInfo processInfo] processIdentifier]);
+}
+
+void section3() {
+	NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:5];
+	[dict setValue: @"http://www.stanford.edu" forKey:@"Stanford University"];
+	[dict setValue: @"http://www.apple.com" forKey:@"Apple"];
+	[dict setValue: @"http://cs193p.stanford.edu" forKey:@"CS193P"];
+	[dict setValue: @"http://itunes.stanford.edu" forKey:@"Stanford on iTunes U"];
+	[dict setValue: @"http://stanfordshop.com" forKey:@"Stanford Mall"];
 	
-    [pool drain];
-    return 0;
+	for (NSString *key in [dict allKeys]) {
+		if ([key hasPrefix:@"Stanford"]) {
+			NSString *url = [dict valueForKey:key];
+			NSLog(@"%@", [NSString stringWithContentsOfURL:[NSURL URLWithString:url]]);
+		} 
+		else {
+			NSLog(@"%@ does not begin with Stanford", key);
+		}
+	}
 }
