5f759d0a1e41b9adbe476497908de2c1d74eef88
John Trupiano <jtrupiano@gmail.com>
2009-08-23 14:17:57 -0400

    Introduce polygon model, initialize and release it properly.

--- Controller.h
+++ Controller.h
@@ -1,10 +1,13 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
+#import "PolygonShape.h"
 
 @interface Controller : NSObject {
     IBOutlet UIButton *decreaseButton;
     IBOutlet UIButton *increaseButton;
     IBOutlet UILabel *numberOfSidesLabel;
+	
+	IBOutlet PolygonShape* myPolygon;
 }
 - (IBAction)decrease;
 - (IBAction)increase;
--- Controller.m
+++ Controller.m
@@ -1,6 +1,12 @@
 #import "Controller.h"
 
 @implementation Controller
+
+- (void)dealloc {
+	[myPolygon release];
+	[super dealloc];
+}
+
 - (IBAction)decrease {
     NSLog(@"I am the decrease function");
 }
@@ -8,4 +14,12 @@
 - (IBAction)increase {
     NSLog(@"I am the increase function");    
 }
+
+- (void)awakeFromNib {
+	myPolygon = [[PolygonShape alloc] init];
+	[myPolygon setNumberOfSides: numberOfSidesLabel.text.integerValue];
+	
+	NSLog(@"My polygon is: %@", myPolygon);
+}
+
 @end
